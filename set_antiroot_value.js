d661fabacc0d1f05b6073c9708825662c966aa18
rlvilla <16118866+rlvilla@users.noreply.github.com>
2023-02-21 14:58:39 +0100

    Add files via upload

---
+++ set_antiroot_value.js
@@ -0,0 +1,14 @@
+Java.perform(function() {
+    // Definición de variable apuntando a la clase "a.a.a.a.a"
+    var class_a = Java.use("a.a.a.a.a");
+
+    // Redefinimos la implementación de la función "a.a.a.a.a.R":
+    class_a.R.implementation = function() {
+        // Capturamos el valor que devuelve la función R original
+        let return_R = this.R();
+        // Imprimimos en la consola el valor original de la función AntiRoot
+        console.log("Valor original =", return_R);
+        // Devolvemos el valor original
+        return false
+    }
+})
